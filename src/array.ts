81bd6255d12328a95eac8e8e18a6cd57f5b5248a
LinhC0918 <nguyenvanlinh12d3@gmail.com>
2018-12-19 08:59:03 +0700

    add new file

---
+++ src/array.ts
@@ -0,0 +1,28 @@
+import './scss/styles.scss';
+/**
+ * Array
+ */
+
+// declare an array
+
+const list: number[] = [1, 2, 3];
+const categories: Array<string> = ['Sport', 'IT', 'Car'];
+console.log(list);
+list.forEach((num) => console.log(num.toFixed(2)));
+console.log(categories);
+categories.forEach((str)=> console.log(str.includes('a')));
+
+// const listSquare = list.map(num => num * num);
+// console.log(listSquare);
+
+// const result = categories.filter(str => str.length > 3)
+// console.log(result);
+
+let x :[string, number];
+x = ['hello', 10];
+console.log(x[0].substr(0));
+// console.log(x[1].substr(1));
+x[3] = 'world';
+x[5] = 100.25;
+
+console.log(x[5].toString());
